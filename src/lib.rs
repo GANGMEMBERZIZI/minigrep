1344075d537fc1d11e495945161121b41402ccec
GANGGMEMBERZIZI <lijaylen86@gmail.com>
2026-01-11 23:02:47 +0800

    优化

--- src/lib.rs
+++ src/lib.rs
@@ -23,12 +23,16 @@ pub struct Config{
     pub ignore_case:bool,//是否忽略大小写
 }
 impl Config{//实现Config的方法
-    pub fn build(args:&[String])->Result<Config,&'static str>{//返回result枚举
-        if args.len()<3{
-            return Err("not enough arguments")//return 提前跳出
-        }
-        let query = args[1].clone(); //0是exe 1是搜索的string 2是路径
-        let file_path =args[2].clone();//这个Config 需要所有权 而 query 和file_path 都是借用 所以要clone
+    pub fn build(mut args:impl Iterator <Item = String>)->Result<Config,&'static str>{//返回result枚举    迭代器next会消耗状态 所以要mut
+        args.next();
+        let query = match args.next(){
+            Some(arg) => arg,
+            None=>return Err("Didnt get a query string"),
+        }; //0是exe 1是搜索的string 2是路径
+        let file_path =match args.next(){
+            Some(arg)=>arg,
+            None=> return Err("Didnt get a file name"),
+        };  //这个Config 需要所有权 而 query 和file_path 都是借用 所以要clone
         let ignore_case=env::var("IGNORE_CASE").is_ok();//检查用户是否设置了名为 IGNORE_CASE 的环境变量，并根据结果返回一个布尔值
         Ok(Config{//最后Ok就是返回值
             query,
@@ -39,23 +43,12 @@ impl Config{//实现Config的方法
 }
 
 pub fn search<'a>(query:&str,contents:&'a str)->Vec<&'a str>{//有多个输入引用，且返回一个引用 要标注声明周期参数(返回跟谁相关) query是关键字 不需要标注 标注content 和返回Vec
-    let mut results=Vec::new();//关键字可能很多行都有 创建一个集合
-    for line in contents.lines(){//line 方法 把每一行返回回来
-        if line.contains(query){
-            results.push(line)
-        }
-    }
-    results
+    contents.lines().filter(|line | line.contains(query))//闭包返回 true -> 保留该元素；返回 false -> 丢弃该元素。  filter 是惰性的（Lazy）。这意味着如果你不调用 .collect() 或不使用 for 循环消费它，它什么都不会做。
+        .collect()
 }
 pub fn search_case_insensitive<'a>(query:&str,contents:&'a str)->Vec<&'a str>{//无大小写的识别
     let query=query.to_lowercase();//把关键字变成小写的
-    let mut results=Vec::new();//关键字可能很多行都有 创建一个集合
-    for line in contents.lines(){//line 方法 把每一行返回回来
-        if line.to_lowercase().contains(&query){
-            results.push(line);
-        }
-    }
-    results
+    contents.lines().filter(|line| line.to_lowercase().contains(&query)).collect()
 }
 #[cfg(test)]
 mod tests{
--- src/main.rs
+++ src/main.rs
@@ -4,13 +4,12 @@ use minigrep::Config;
 //不产生panic
 
 fn main() {
-    let args:Vec<String>=env::args().collect();
     // dbg!(&args);会显示路径和内容，会打印到stderr
-    let config = Config::build(&args).unwrap_or_else(|err|{
+    let config = Config::build(env::args()).unwrap_or_else(|err|{
         eprintln!("problem parsing arguments {}",err);//打印错误信息 stderr错误信息即使 >output.txt 也显示终端上 用于区分信息
         process::exit(1);//停止程序 1是错误退出
 
-    });
+    });//闭包
     println!("searching {}",config.query);
     println!("in file {}",config.file_path);
     if let Err(e)=minigrep::run(config){
